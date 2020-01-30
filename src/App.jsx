5d7e517bd8f8ac0e6a02a6e41fb3ebeeebadad0b
Cornelius <55963357+Corneeltron@users.noreply.github.com>
2020-01-30 13:43:32 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,18 +25,6 @@ const App = () => {
     );
   } else {
     return (
-      /** 
-       * Wrap the components that we want to share the unique states with (ex: share testCase state with navbar & left panel (the two containers that hold the components that need testCaseRducer)) in the unique providers (ex: TestCaseContext.Provider). 
-       * You can only provide one value to a Provider. 
-       *  - In order to avoid creating separate Contexts, wrap multiples in an array (ex: testCase and dispatchToTestCase).
-       * 
-       * 
-       * NOTE: This concept is similar to Redux and how it provides the store to your top-level component and all of its children. 
-       * We just have to create separate providers for each reducer because we don’t have a global store ala Redux.
-       * 
-       * 
-       * We access the value that we gave to the Provider through useContext
-       */
       <div id={global.isFileDirectoryOpen ? styles.appGridOpen : styles.appGridClose}>
         <GlobalContext.Provider value={[global, dispatchToGlobal]}>
           {/* value wrapped in array since Provider only takes in one value */}
